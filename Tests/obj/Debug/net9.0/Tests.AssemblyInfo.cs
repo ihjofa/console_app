906e21cf94fa6140d0ac6a899b04cba1b435da48
ihjofa <zuomuqingye46@gmail.com>
2025-11-15 22:18:44 +0900

    testsがmodelを参照できるようにした

--- Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd8956690dc3e4dbf67e8658e278b00e52c82ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e47d15644d56e5b0e14c794c1683ce50a5716d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
